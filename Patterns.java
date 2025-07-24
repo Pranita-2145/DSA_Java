af824c8f346f069af1eed3b16b50cb911b935c26
Pranita Bhor <bpranita2005@gmail.com>
2025-07-24 16:05:55 +0530

    Patterns added

---
+++ Patterns.java
@@ -0,0 +1,13 @@
+package DSA_Java;
+
+public class Patterns {
+    public static void main(String[] args) {
+        //Star Pattern
+        for(int i=0; i<=4; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
