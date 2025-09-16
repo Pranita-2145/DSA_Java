db36990685744eda179bc0a29b5db6853f20c3d8
Pranita Bhor <bpranita2005@gmail.com>
2025-09-16 16:23:27 +0530

    Bit day 1

---
+++ Bitwise.java
@@ -0,0 +1,11 @@
+package DSA_Java;
+import java.util.*;
+public class Bitwise {
+    public static void main(String[] args) {
+        System.out.println((5 & 6)); // AND
+        System.out.println((5 | 6)); //OR
+        System.out.println((5 ^ 6)); //XOR
+        System.out.println((~5)); //NOT
+        System.out.println((5<<3));// Bitwise left shift
+    }
+}
